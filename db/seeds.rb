2a0223c7a70a72c229a5e902b20966e744d0a608
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-15 22:11:58 +0200

    Finished rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+10.times do
+  Flat.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph_by_chars(1500, false),
+    price_per_night: [*60..300].sample,
+    number_of_guests: [*1..6].sample,
+    picture_url: Faker::LoremPixel.image("#{[*600..1600].sample}x#{[*600..1600].sample}")
+  )
+end
